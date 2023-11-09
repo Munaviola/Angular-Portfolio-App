78bfff229523dcddf4e75686aaa26530b57d915f
Munaviola <munaviola5@gmail.com>
2023-11-09 12:08:39 +0100

    enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Hello-World';
+  userData = {
+    user: '',
+  };
+
+  submit() {
+    const formData = this.userData;
+    const message = `Name: ${formData.user}`;
+    alert(message);
+  }
 }
